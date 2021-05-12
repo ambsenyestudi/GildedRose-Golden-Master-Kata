691d3512c9d46c5dffa69e64e362b39fc27f9a65
mariojespada <46303116+mariojespada@users.noreply.github.com>
2021-05-12 20:06:14 +0200

    DowngradeQuality refactor

--- src/GildedRose/GildedRose/GildedRose.cs
+++ src/GildedRose/GildedRose/GildedRose.cs
@@ -20,13 +20,7 @@ namespace GildedRose
             {
                 if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
+                    DowngradeQuality(Items[i]);
                 }
                 else
                 {
@@ -89,5 +83,14 @@ namespace GildedRose
                 }
             }
         }
+
+        private void DowngradeQuality(Item item)
+        {
+            if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                item.Quality = item.Quality - 1;
+            }
+        }
+
     }
 }
