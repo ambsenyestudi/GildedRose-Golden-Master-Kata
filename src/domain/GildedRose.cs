79ad251b4c4da31880282fd07c433f7134aab199
ambsenyestudi <adrifre7@hotmail.com>
2022-09-04 13:13:52 +0200

    Extracts backstage to a constant

--- src/domain/GildedRose.cs
+++ src/domain/GildedRose.cs
@@ -2,6 +2,8 @@
 public class GildedRose
 {
     public const string SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public const string AGED_BRIE = "Aged Brie";
+    public const string BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
     public IList<Item> Items { get; }
     public GildedRose(IList<Item> Items)
     {
@@ -12,7 +14,7 @@ public class GildedRose
     {
         for (var i = 0; i < Items.Count; i++)
         {
-            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            if (Items[i].Name != AGED_BRIE && Items[i].Name != BACKSTAGE)
             {
                 if (Items[i].Quality > 0)
                 {
@@ -28,7 +30,7 @@ public class GildedRose
                 {
                     IncreaseQuallity(Items[i]);
 
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                    if (Items[i].Name == BACKSTAGE)
                     {
                         if (Items[i].SellIn < 11)
                         {
@@ -56,9 +58,9 @@ public class GildedRose
 
             if (Items[i].SellIn < 0)
             {
-                if (Items[i].Name != "Aged Brie")
+                if (Items[i].Name != AGED_BRIE)
                 {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (Items[i].Name != BACKSTAGE)
                     {
                         if (Items[i].Quality > 0)
                         {
